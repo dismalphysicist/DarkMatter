5336db3632e5f48e44f864383174304945156360
dismalphysicist <46093766+dismalphysicist@users.noreply.github.com>
2019-08-28 21:39:13 +0100

    Removed unnecessary print statements and added a clock

--- integration.cpp
+++ integration.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 #include <valarray>
 #include <random>
-#include <time.h>
+#include <ctime>
 #include "integrand.h"
 #include "expo_fit.h"
 
@@ -24,29 +24,24 @@ double monteCarloIntegrate(int N, int rounds, double &err, double start=start_po
     double norm_factor = 1.0/area;
     cout << "Normalisation factor = " << norm_factor << endl;
 
+    //setting up random number generator 
+    random_device rd;     // only used once to initialise (seed) engine
+    mt19937 rng(rd());    // random-number engine used (Mersenne-Twister in this case)
+    uniform_real_distribution<double> uni(fit.rho1(start),fit.rho1(end)); // guaranteed unbiased
+
     for (int i = 0; i < rounds; i++) {
+        
         //initialising array of rhos with random numbers 
         valarray<double> rhos = valarray<double>(N);
-
-        random_device rd;     // only used once to initialise (seed) engine
-        mt19937 rng(rd());    // random-number engine used (Mersenne-Twister in this case)
-        uniform_real_distribution<double> uni(fit.rho1(start),fit.rho1(end)); // guaranteed unbiased
-
         for (int i=0; i<N; i++) {
             rhos[i] = uni(rng);
         }
-        for(double i : rhos) cout << i << ", ";
-        cout << endl;
 
         //getting suitably distributed s values from uniform rhos 
         valarray<double> ss = fit.s1(rhos);
-        for(double i : ss) cout << i << ", ";
-        cout << endl;
 
         //y = f/g with normalisation factor 
         valarray<double> ys = f(ss)/(norm_factor * fit.g1(ss));
-        for(double i : ys) cout << i << ", ";
-        cout << endl;
 
         //summing up arrays, keeping running total
         sum_y += ys.sum();
@@ -61,9 +56,16 @@ double monteCarloIntegrate(int N, int rounds, double &err, double start=start_po
 }
 
 int main() {
-    cout << f(5) << endl;
+    //cout << f(5) << endl;
     double err;
-    cout << monteCarloIntegrate(100,1,err) << endl;
+
+    clock_t begin = clock();
+
+    cout << monteCarloIntegrate(1000000,100,err) << endl;
+
+    clock_t end = clock();
+
     cout << err << endl;
+    cout << double(end-begin)/CLOCKS_PER_SEC << " s" << endl;
     return 0;
 }
